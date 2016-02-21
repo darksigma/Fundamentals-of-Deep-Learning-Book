436745b60f38e891343d7f475ad548de9b444a25
Nikhil <nkbuduma@gmail.com>
2016-02-20 23:21:55 -0800

    more stuff

--- convnet_cifar.py
+++ convnet_cifar.py
@@ -176,22 +176,22 @@ if __name__ == '__main__':
                         avg_cost += new_cost/total_batch
                         print "Epoch %d, minibatch %d of %d. Cost = %0.4f." %(epoch, i, total_batch, new_cost)
                     
-                #     # Display logs per epoch step
-                #     if epoch % display_step == 0:
-                #         print "Epoch:", '%04d' % (epoch+1), "cost =", "{:.9f}".format(avg_cost)
+                    # Display logs per epoch step
+                    if epoch % display_step == 0:
+                        print "Epoch:", '%04d' % (epoch+1), "cost =", "{:.9f}".format(avg_cost)
 
-                #         accuracy = sess.run(eval_op, feed_dict={keep_prob: 1, train_or_eval: -1.0})
+                        accuracy = sess.run(eval_op, feed_dict={keep_prob: 1, train_or_eval: 1.0})
 
-                #         print "Validation Error:", (1 - accuracy)
+                        print "Validation Error:", (1 - accuracy)
 
-                #         summary_str = sess.run(summary_op, feed_dict={keep_prob: 0.5, train_or_eval: 1.0})
-                #         summary_writer.add_summary(summary_str, sess.run(global_step))
+                        summary_str = sess.run(summary_op, feed_dict={keep_prob: 0.5, train_or_eval: 1.0})
+                        summary_writer.add_summary(summary_str, sess.run(global_step))
 
-                #         saver.save(sess, "conv_cifar_logs/model-checkpoint", global_step=global_step)
+                        saver.save(sess, "conv_cifar_logs/model-checkpoint", global_step=global_step)
 
 
-                # print "Optimization Finished!"
+                print "Optimization Finished!"
 
-                # accuracy = sess.run(eval_op, feed_dict={keep_prob: 1, train_or_eval: -1.0})
+                accuracy = sess.run(eval_op, feed_dict={keep_prob: 1, train_or_eval: 1.0})
 
-                # print "Test Accuracy:", accuracy
+                print "Test Accuracy:", accuracy
