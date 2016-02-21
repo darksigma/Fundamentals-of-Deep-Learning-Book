cc12a6c8ca434d105ce01f2c4a61c46421860c00
Nikhil <nkbuduma@gmail.com>
2016-02-20 21:06:38 -0800

    changed var name

--- convnet_cifar.py
+++ convnet_cifar.py
@@ -132,10 +132,10 @@ if __name__ == '__main__':
 
             with tf.variable_scope("cifar_conv_model"):
 
-                train_or_eval = tf.placeholder(tf.int32) # placeholder for whether to pull from train or val data
+                # train_or_eval = tf.placeholder(tf.int32) # placeholder for whether to pull from train or val data
                 keep_prob = tf.placeholder(tf.float32) # dropout probability
 
-                x, y = tf.cond(tf.equal(train_or_eval, tf.constant(1, dtype=tf.int32)), distorted_inputs, inputs)
+                x, y = tf.cond(tf.equal(tf.constant(1, dtype=tf.int32), tf.constant(1, dtype=tf.int32)), distorted_inputs, inputs)
 
                 output = inference(x, keep_prob)
 
@@ -171,7 +171,7 @@ if __name__ == '__main__':
                     # Loop over all batches
                     for i in range(total_batch):
                         # Fit training using batch data
-                        _, new_cost = sess.run([train_op, cost], feed_dict={train_or_eval: np.int32(1), keep_prob: 0.5})
+                        _, new_cost = sess.run([train_op, cost], feed_dict={keep_prob: 0.5})
                         # Compute average loss
                         avg_cost += new_cost/total_batch
                         print "Epoch %d, minibatch %d of %d. Average cost = %0.4f." %(epoch, i, total_batch, avg_cost)
